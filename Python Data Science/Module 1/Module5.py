6ef637463da94567af4df528b75e7bf405bfc28b
developer_deepak <hope.deepak2@gmail.com>
2024-01-27 23:22:02 -0600

    module5 example2

--- Python Data Science/Module 1/Module5.py
+++ Python Data Science/Module 1/Module5.py
@@ -21,3 +21,18 @@ def add(num1: int, num2: int) -> int:
 num1, num2 = 5, 15
 ans = add(num1, num2)
 print(f"The addition of {num1} and {num2} results {ans}.")
+
+
+# some more functions
+def is_prime(n):
+	if n in [2, 3]:
+		return True
+	if (n == 1) or (n % 2 == 0):
+		return False
+	r = 3
+	while r * r <= n:
+		if n % r == 0:
+			return False
+		r += 2
+	return True
+print(is_prime(78), is_prime(79))
