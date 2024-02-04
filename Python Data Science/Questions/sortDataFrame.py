634cc2d8c26d38f9858e5939aaf76b1b38e77572
karkideepak <hope.deepak2@gmail.com>
2024-02-03 22:38:16 -0600

    exercises

---
+++ Python Data Science/Questions/sortDataFrame.py
@@ -0,0 +1,10 @@
+df = pd.DataFrame([["A", 1], ["B", 2],
+                   ["C", 3], ["D", 4]], columns=["col1", "col2"])
+
+sort_list = ["C", "A", "D", "B"]
+
+## start your code below
+new_df = ... 
+## end your code here
+
+print(new_df)
